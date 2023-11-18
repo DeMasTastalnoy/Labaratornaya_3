21f8e867dd94dd6b88f516b86296274419446e71
FFF5123 <dima.stankov2004@yandex.ru>
2023-11-18 03:26:08 +0300

    Second_commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Stankov Dima IDB-22-10")
+import re
+
+
+def check_domain_name(input_string):
+    pattern = re.compile(r'^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+(?:[a-zA-Z]{2,})$', re.VERBOSE)
+    return bool(re.match(pattern, input_string))
+
+
+input_string = "stankin.rus.moscow.www"
+print(check_domain_name(input_string))
+input_string = "stankin..www"
+print(check_domain_name(input_string))
